f57dbc830995cb94918e34d5ddab506faa9ca109
andreymlv <ndrmlv@ya.ru>
2022-11-17 21:00:16 +0300

    Add task ui stub.

--- app/src/main/java/ru/ndrmlv/todo/MainActivity.kt
+++ app/src/main/java/ru/ndrmlv/todo/MainActivity.kt
@@ -3,13 +3,17 @@ package ru.ndrmlv.todo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ru.ndrmlv.todo.ui.theme.TodoTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +21,56 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TodoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                MyApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp() {
+    // A surface container using the 'background' color from the theme
+    Surface(
+        modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
+    ) {
+        Column {
+            Column(
+                modifier = Modifier
+                    .verticalScroll(rememberScrollState())
+                    .weight(1f)
+            ) {
+                Task(false, "test")
+                Task(false, "test")
+                Task(false, "test")
+                Task(false, "test")
+                Task(false, "test")
+                Task(true, "test")
+                Task(false, "test")
+                Task(false, "test")
+                Task(true, "test")
+                Task(false, "test")
+                Task(false, "test")
+                Task(false, "test")
+                Task(true, "test")
+                Task(false, "test")
+                Task(false, "test")
+                Task(true, "test")
+                Task(false, "test")
+                Task(false, "test")
+                Task(true, "test")
+                Task(false, "test")
+                Task(false, "test")
+                Task(true, "test")
+                Task(false, "test")
+                Task(false, "test")
+            }
+            Box(
+                modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.BottomCenter
+            ) {
+                Button(
+                    onClick = { /*TODO*/ }, shape = CircleShape, modifier = Modifier.size(48.dp)
                 ) {
-                    Greeting("Android")
+                    Text(text = "+")
                 }
             }
         }
@@ -30,14 +78,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Task(completed: Boolean, content: String) {
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth(), shape = RoundedCornerShape(16.dp)
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Checkbox(checked = completed, onCheckedChange = { /*TODO*/ })
+            Text(text = content)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+private fun DefaultPreview() {
     TodoTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
