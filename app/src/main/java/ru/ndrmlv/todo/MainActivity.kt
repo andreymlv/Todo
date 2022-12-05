45df4ca1df8dfa3a1337b05ce36019d96302c8df
andreymlv <ndrmlv@ya.ru>
2022-12-05 22:33:31 +0300

    Fix `LazyColumn` correct caching. Add some onClick behavior.

--- app/src/main/java/ru/ndrmlv/todo/MainActivity.kt
+++ app/src/main/java/ru/ndrmlv/todo/MainActivity.kt
@@ -3,12 +3,16 @@ package ru.ndrmlv.todo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,18 +37,69 @@ fun MyApp() {
     val tasks = listOf(
         TaskGroup(
             Task(
-                false, "task", null, null
+                remember { mutableStateOf(false) }, "task root without sub", "test", null
             ), emptyList()
         ), TaskGroup(
             Task(
-                false, "task", "desc", null
+                remember { mutableStateOf(false) }, "task root", "desc 1", null
             ), listOf(
                 Task(
-                    false, "task", "desc", null
+                    remember { mutableStateOf(false) }, "task sub", "desc 2", null
+                )
+            )
+        ), TaskGroup(
+            Task(
+                remember { mutableStateOf(false) }, "task root without sub", "test", null
+            ), emptyList()
+        ), TaskGroup(
+            Task(
+                remember { mutableStateOf(false) }, "task root", "desc 1", null
+            ), listOf(
+                Task(
+                    remember { mutableStateOf(false) }, "task sub", "desc 2", null
+                )
+            )
+        ), TaskGroup(
+            Task(
+                remember { mutableStateOf(false) }, "task root without sub", "test", null
+            ), emptyList()
+        ), TaskGroup(
+            Task(
+                remember { mutableStateOf(false) }, "task root", "desc 1", null
+            ), listOf(
+                Task(
+                    remember { mutableStateOf(false) }, "task sub", "desc 2", null
+                )
+            )
+        ), TaskGroup(
+            Task(
+                remember { mutableStateOf(false) }, "task root without sub", "test", null
+            ), emptyList()
+        ), TaskGroup(
+            Task(
+                remember { mutableStateOf(false) }, "task root", "desc 1", null
+            ), listOf(
+                Task(
+                    remember { mutableStateOf(false) }, "task sub", "desc 2", null
+                )
+            )
+        ), TaskGroup(
+            Task(
+                remember { mutableStateOf(false) }, "task root without sub", "test", null
+            ), emptyList()
+        ), TaskGroup(
+            Task(
+                remember { mutableStateOf(false) }, "task root", "desc 1", null
+            ), listOf(
+                Task(
+                    remember { mutableStateOf(false) }, "task sub", "desc 2", null
                 )
             )
         )
     )
+    val foldCompleted = remember {
+        mutableStateOf(true)
+    }
     Surface(
         modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
     ) {
@@ -64,11 +119,43 @@ fun MyApp() {
                 LazyColumn(modifier = Modifier.padding(paddingValues)) {
                     // Uncompleted with subtasks offset
                     items(tasks) { task ->
-                        task.View(8.dp)
+                        task.View(32.dp)
                     }
                     // Completed flat without damn offset
-                    items(tasks) { task ->
-                        task.complete().View()
+                    item {
+                        Card(
+                            modifier = Modifier.fillMaxWidth(), shape = RoundedCornerShape(16.dp)
+                        ) {
+                            Row {
+                                Row(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(24.dp)
+                                        .clickable(onClick = {
+                                            foldCompleted.value = !foldCompleted.value
+                                        })
+                                ) {
+                                    Column {
+                                        Text(text = "Completed (number of completed tasks)")
+                                    }
+                                    Column(
+                                        modifier = Modifier.fillMaxWidth(),
+                                        horizontalAlignment = Alignment.End
+                                    ) {
+                                        if (!foldCompleted.value) {
+                                            Text(text = "^")
+                                        } else {
+                                            Text(text = "v")
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    if (!foldCompleted.value) {
+                        items(tasks) { task ->
+                            task.complete().View()
+                        }
                     }
                 }
             },
--- app/src/main/java/ru/ndrmlv/todo/model/Task.kt
+++ app/src/main/java/ru/ndrmlv/todo/model/Task.kt
@@ -5,13 +5,15 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.sp
 import java.util.*
 
 class Task(
-    private val completed: Boolean,
+    public val completed: MutableState<Boolean>,
     private val task: String,
     private val desc: String?,
     private val alarmedAt: Date?,
@@ -19,18 +21,20 @@ class Task(
     private val updatedAt: Date = Date(),
     private val uid: UUID = UUID.randomUUID(),
 ) {
-    fun toggle(): Task {
-        return Task(!completed, task, desc, alarmedAt, createdAt, Date(), uid)
+    private fun toggle(): Task {
+        return Task(mutableStateOf(!completed.value), task, desc, alarmedAt, createdAt, Date(), uid)
     }
 
     fun complete(): Task {
-        return Task(true, task, desc, alarmedAt, createdAt, Date(), uid)
+        return Task(mutableStateOf(true), task, desc, alarmedAt, createdAt, Date(), uid)
     }
 
     @Composable
     fun View() {
         Row(verticalAlignment = Alignment.CenterVertically) {
-            Checkbox(checked = completed, onCheckedChange = { /*TODO*/ })
+            Checkbox(
+                checked = completed.value,
+                onCheckedChange = { completed.value = toggle().completed.value })
             Column {
                 Text(text = task)
                 desc?.also {
